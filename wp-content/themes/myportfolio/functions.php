800d511a4c53534b4e71b4591e9967d4d473bba3
tarosjk <tarosjk@gmail.com>
2024-09-05 11:02:33 +0900

    抜粋文の省略記号を変更

--- wp-content/themes/myportfolio/functions.php
+++ wp-content/themes/myportfolio/functions.php
@@ -10,3 +10,15 @@ function my_theme_support()
   add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'my_theme_support');
+
+
+/**
+ * 抜粋文の省略記号を変更
+ * 
+ * @return string 省略記号
+ */
+function my_excerpt_ellipsis()
+{
+  return ' ...';
+}
+add_filter('excerpt_more', 'my_excerpt_ellipsis');
