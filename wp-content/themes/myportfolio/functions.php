a3106be0a9741428d4aa6e539e27cfb985dc7760
tarosjk <tarosjk@gmail.com>
2024-09-05 14:26:10 +0900

    スタイル、スクリプトの読み込みをfunctions.phpに追加

--- wp-content/themes/myportfolio/functions.php
+++ wp-content/themes/myportfolio/functions.php
@@ -9,6 +9,7 @@ function my_theme_support()
 {
   add_theme_support('post-thumbnails'); //アイキャッチ画像
   add_theme_support('menus'); //メニュー（ナビゲーション）
+  add_theme_support('title-tag'); //いい感じのtitleタグを出力
 }
 add_action('after_setup_theme', 'my_theme_support');
 
@@ -23,3 +24,30 @@ function my_excerpt_ellipsis()
   return ' ...';
 }
 add_filter('excerpt_more', 'my_excerpt_ellipsis');
+
+
+
+function my_enqueue_scripts()
+{
+  // スタイル
+  wp_enqueue_style(
+    'my-style',
+    get_template_directory_uri() . '/assets/css/style.css',
+    [],
+    filemtime(get_template_directory() . '/assets/css/style.css')
+  );
+
+  // スクリプト
+  wp_enqueue_script('jquery');
+  wp_enqueue_script(
+    'my-script',
+    get_template_directory_uri() . '/assets/js/script.js',
+    ['jquery'],
+    filemtime(get_template_directory() . '/assets/js/script.js'),
+    [
+      'strategy' => 'defer',
+      // 'in_footer' => true // wp_footerの実行時に読み込まれる
+    ]
+  );
+}
+add_action('wp_enqueue_scripts', 'my_enqueue_scripts');
